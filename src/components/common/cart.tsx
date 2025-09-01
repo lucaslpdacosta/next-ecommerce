1b0a8c037234f0769fc5ad17ae59ee6eae86dd27
Lucas Prudencio <lucaslpdacosta1@gmail.com>
2025-08-31 23:36:26 -0300

    feat: added address form

--- src/components/common/cart.tsx
+++ src/components/common/cart.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ShoppingBasketIcon } from "lucide-react";
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { formatCentsToBRL } from "@/helpers/money";
 import { useCart } from "@/hooks/queries/use-cart";
@@ -74,7 +75,9 @@ export const Cart = () => {
                 <p>{formatCentsToBRL(cart?.totalPriceInCents ?? 0)}</p>
               </div>
 
-              <Button className="mt-5 rounded-full">Finalizar compra</Button>
+              <Button className="mt-5 rounded-full" asChild>
+                <Link href="/cart/identification">Finalizar compra</Link>
+              </Button>
             </div>
           )}
         </div>
@@ -82,5 +85,3 @@ export const Cart = () => {
     </Sheet>
   );
 };
-
-// SERVER ACTION
