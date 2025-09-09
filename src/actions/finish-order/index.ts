afe74908dba6ac864c9bd2d0e8b1fe179a2629cc
Lucas Prudencio <lucaslpdacosta1@gmail.com>
2025-09-08 21:30:43 -0300

    feat: added stripe integration

--- src/actions/finish-order/index.ts
+++ src/actions/finish-order/index.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { eq } from "drizzle-orm";
+import { revalidatePath } from "next/cache";
 import { headers } from "next/headers";
 import { db } from "@/db";
 import {
@@ -40,6 +41,7 @@ export const finishOrder = async () => {
     (acc, item) => acc + item.productVariant.priceInCents * item.quantity,
     0,
   );
+  let orderId: string | undefined;
   await db.transaction(async (tx) => {
     if (!cart.shippingAddress) {
       throw new Error("Shipping address not found");
@@ -67,6 +69,7 @@ export const finishOrder = async () => {
     if (!order) {
       throw new Error("Failed to create order");
     }
+    orderId = order.id;
     const orderItemsPayload: Array<typeof orderItemTable.$inferInsert> =
       cart.items.map((item) => ({
         orderId: order.id,
@@ -78,4 +81,8 @@ export const finishOrder = async () => {
     await tx.delete(cartTable).where(eq(cartTable.id, cart.id));
     await tx.delete(cartItemTable).where(eq(cartItemTable.cartId, cart.id));
   });
+  if (!orderId) {
+    throw new Error("Failed to create order");
+  }
+  return { orderId };
 };
